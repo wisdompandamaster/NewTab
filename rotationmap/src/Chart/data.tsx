8cf20e5389050d965ff0be02848f4f489b409ca6
caochen <1124904301@qq.com>
2022-02-10 16:47:23 +0800

    feat:点击图片弹框显示图片列表

---
+++ rotationmap/src/Chart/data.tsx
@@ -0,0 +1,19 @@
+const data = [
+    {
+        src: 'pic1',
+    },
+    {
+        src: 'pic2',
+    },
+    {
+        src: 'pic3',
+    },
+    {
+        src: 'pic4',
+    },
+    {
+        src: 'pic5',
+    },
+];
+
+export default data;
--- rotationmap/src/Chart/index.tsx
+++ rotationmap/src/Chart/index.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
-import { Carousel } from 'antd';
+import React, { useState } from 'react';
+import { Carousel, Modal, Table, Upload, Button, Image } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
+import data from './data';
 import './style.css';
-import imgs from './data';
 
 const Chart: React.FC = () => {
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const showModal = () => {
+        setModalVisible(true);
+    }
+
+    const columns = [
+        {
+            title: '图片',
+            dataIndex: 'src',
+            key: 'src',
+            render: (text: any) => {
+                return (
+                    <>
+                        <Image width={50} height={50} alt={text} src={require(`../asset/${text}.jpg`)} />
+                    </>
+                );
+            }
+        },
+        {
+            title: '操作',
+            dataIndex: 'action',
+            key: 'action',
+            render: () => {
+                return (
+                    <>
+                        <Button danger>删除</Button>
+                    </>
+                );
+            }
+        }
+    ];
 
     const settings = {
         dots: true,
@@ -17,15 +50,31 @@ const Chart: React.FC = () => {
         <>
             <Carousel {...settings} className='carousel'>
                 {
-                    imgs.map((item, index) => {
+                    data.map((item, index) => {
                         return (
-                            <div className='panel' key={index}>
-                                <img width='100%' alt={item.alt} src={item.src} />
+                            <div className='panel' key={index} onClick={showModal}>
+                                <img
+                                    width='100%'
+                                    alt={item.src}
+                                    // src={item.src} 
+                                    src={require(`../asset/${item.src}.jpg`)}
+                                />
                             </div>
                         )
                     })
                 }
             </Carousel>
+            <Modal
+                title='图片墙'
+                visible={modalVisible}
+                onCancel={() => { setModalVisible(false) }}
+                footer={null}
+            >
+                <Table dataSource={data} columns={columns} pagination={false} scroll={{ y: 300 }} />
+                <Upload>
+                    <Button icon={<UploadOutlined />} type='primary' style={{ marginTop: '10px' }}>添加图片</Button>
+                </Upload>
+            </Modal>
         </>
     )
 }
