20b7bd735c1bad6db022fabf2512478ae41ff068
HYY <885543243@qq.com>
2022-02-15 13:54:45 +0800

    feat: 基本完成

--- notes/src/App.js
+++ notes/src/App.js
@@ -3,19 +3,29 @@ import {Button} from 'antd';
 import Notes from "./notes";
 
 const App = () => {
-    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [isNotesVisible, setNotesVisible] = useState(false);
 
     // 是否显示弹框
     const isShowModal = () => {
-        setIsModalVisible(!isModalVisible);
+        setNotesVisible(!isNotesVisible);
     };
 
     return (
         <>
-            <Button type="primary" onClick={isShowModal}>Open Modal</Button>
-            <Notes isModalVisible={isModalVisible} isShowModal={isShowModal}/>
+            <div className='note-card' onClick={isShowModal}>
+                <header className="note-header">
+                    <div className="note-icon"></div>
+                    <h1 className="note-title">笔记</h1>
+                </header>
+
+                <div className='note-body'>
+                    {/*只能有四条*/}
+                    <span className='note-content'>123</span>
+                </div>
+            </div>
+            <Notes isModalVisible={isNotesVisible} isShowModal={isShowModal}/>
         </>
     );
-}
+};
 
 export default App
