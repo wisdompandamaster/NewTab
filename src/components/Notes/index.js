eb21badd7ca5c5c60f8f78eee93fbdedeb0259c3
StarkerRegen <lszviima@yeah.net>
2022-03-01 09:03:59 +0800

    Revert "fix: 笔记修复"
    This reverts commit a826b7ae9c849a1ba5876643367b9bfbe117d476.

--- src/components/Notes/index.js
+++ src/components/Notes/index.js
@@ -19,8 +19,7 @@ const Notes = () => {
     };
     // 添加笔记
     const isNoteAdd = () => {
-        if (notesData.length === 0) setNoteIndex(0)
-        const data = notesData.concat([{id:nanoid(), value: '无内容', text:""}]);
+        const data = notesData.concat([{id:nanoid(), value: '无内容'}]);
         setNotesData(data);
         setNotesList(data);
     };
@@ -29,14 +28,14 @@ const Notes = () => {
         <>
             <div className='note-card' onClick={isShowModal}>
                 <header className="note-header">
-                    <div className="note-icon"/>
+                    <div className="note-icon"></div>
                     <h1 className="note-title">笔记</h1>
                 </header>
                 <div className='note-body'>
-                    <div  className='note-content'>{ notesData[0]?.value }</div>
-                    <div  className='note-content'>{notesData[1]?.value}</div>
-                    <div  className='note-content'>{notesData[2]?.value}</div>
-                    <div  className='note-content'>{notesData[3]?.value}</div>
+                    <div  className='note-content'>{ notesData[0]?.html }</div>
+                    <div  className='note-content'>{notesData[1]?.html}</div>
+                    <div  className='note-content'>{notesData[2]?.html}</div>
+                    <div  className='note-content'>{notesData[3]?.html}</div>
                 </div>
             </div>
             <Modal title={<div style={{fontSize:'25px',fontWeight:'500',letterSpacing:'8px',marginLeft:'24px'}}>笔记</div>}
@@ -48,13 +47,11 @@ const Notes = () => {
                     <div className='notes-tabs'>
                         <div className='notes-tabs-body'>
                             {
-                                notesData.length !== 0
-                                    ? notesData.map((item, index) => {
-                                        return (
-                                            <NoesTabs key={index} noteIndex={noteIndex} notesData={notesData} setNotesList={setNotesList} setNotesData={setNotesData} setNoteIndex={setNoteIndex} item={item} index={index}/>
-                                        )
-                                    })
-                                    : <p  className='wrapper'>快来添加第一条笔记吧</p>
+                                notesData.map((item, index) => {
+                                    return (
+                                        <NoesTabs key={index} notesData={notesData} setNotesList={setNotesList} setNotesData={setNotesData} setNoteIndex={setNoteIndex} item={item} index={index}/>
+                                    )
+                                })
                             }
                         </div>
                         <Button className='notes-add'
--- src/components/Notes/noesTabs.js
+++ src/components/Notes/noesTabs.js
@@ -4,7 +4,7 @@ import {DeleteOutlined, ExclamationCircleOutlined} from "@ant-design/icons";
 import Meta from "antd/es/card/Meta";
 
 const NoesTabs =(data) =>{
-    const [isCardDelete, setIsCardDelete] = useState(false); // 删除按钮
+    const [isCardDelete, setIsCardDelete] = useState(false);
 
     // 是否显示删除按钮
     const handleMouse = (flag) => {
@@ -15,18 +15,19 @@ const NoesTabs =(data) =>{
         data.setNoteIndex(index)
     };
     // 删除笔记
-    const isDelete = (item,index) => {
+    const isDelete = (item) => {
         Modal.confirm({
             title: '提示',
             icon: <ExclamationCircleOutlined/>,
-            content: '删除这条笔记?',
+            content: '删除这条备忘录?',
             okText: '确认',
             cancelText: '取消',
             onOk: function () {
                 const newNotesData = data.notesData.filter(i  => i.id !== item.id)
                 data.setNotesData(newNotesData)
                 data.setNotesList(newNotesData)
-                data.setNoteIndex(index-1)
+
+                console.log("确认",item)
             },
             onCancel: function () {
                 console.log("取消")
@@ -34,24 +35,21 @@ const NoesTabs =(data) =>{
         });
     };
     return (
-        <>
-            <Card
-                className={ data?.index === data.noteIndex ? 'notes-tabs-item notes-tabs-checked' : 'notes-tabs-item'}
-                onMouseEnter={() => handleMouse(true)}
-                onMouseLeave={() => handleMouse(false)}
-                onClick={() => isShowNotes(data.item,data.index)}
-                hoverable={true}>
-                <Meta title={data.item.value}/>
-                <Button
-                    className='notes-del'
-                    onClick={() => isDelete(data.item,data.index)}
-                    type="text"
-                    style={{display: isCardDelete ? 'block' : 'none'}}
-                    shape="circle"
-                    icon={<DeleteOutlined/>}/>
-            </Card>
-        </>
-
+        <Card
+            onMouseEnter={() => handleMouse(true)}
+            onMouseLeave={() => handleMouse(false)}
+            onClick={e => isShowNotes(data.item,data.index)}
+            className='notes-tabs-item'
+            hoverable={true}>
+            <Meta title={data.item.value}/>
+            <Button
+                className='notes-del'
+                onClick={() => isDelete(data.item)}
+                type="text"
+                style={{display: isCardDelete ? 'block' : 'none'}}
+                shape="circle"
+                icon={<DeleteOutlined/>}/>
+        </Card>
     )
 
 };
