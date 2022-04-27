8fc1dd41930378d5422d757c75fdb8fb60740ac8
wisdompandamaster <2532569059@qq.com>
2022-04-27 23:51:25 +0800

    add function competition

---
+++ src/components/Competition/Competition.js
@@ -0,0 +1,11 @@
+import './Competition.css'
+
+export default function Competition(){
+    return (
+        <>
+        <div className='competition'>
+            <div className='left'><div></div><p>比赛信息</p></div>
+        </div>
+        </>
+    )
+}
--- src/components/FunctionAera/FunctionAera.js
+++ src/components/FunctionAera/FunctionAera.js
@@ -7,13 +7,14 @@ import Notes from "../Notes";
 import Weather from '../Weather/Weather'
 import CalComponent from '../Calendar/CalComponent'
 import Apps from '../Apps/Apps'
+import Competition from '../Competition/Competition'
 import {SortableContainer, SortableElement} from 'react-sortable-hoc'
 import {arrayMoveImmutable} from 'array-move'
 import { useSelector } from 'react-redux'
 import { useState } from 'react'
 
 
-const funcs = [<News/>,<Todo/>,<Pictures/>,<Notes/>,<Weather/>,<CalComponent/>,<Apps/>]
+const funcs = [<News/>,<Todo/>,<Pictures/>,<Notes/>,<Weather/>,<CalComponent/>,<Apps/>,<Competition/>]
 
 const SortableItem = SortableElement(({value}) => <div className='sortableItem'>{funcs[value]}</div>);
 const SortableList = SortableContainer(({items}) => {
@@ -32,7 +33,7 @@ export default function FunctionAera(){   //中间的功能组件，放在里面
     //先重新排列这些组件，统一组件大小，再拖拽排序 4.21待解决
     const clear = useSelector(state=>state.clear)
     let display = clear? 'none':'block'
-    let functionList = localStorage.getItem('functionList')? localStorage.getItem('functionList'):"[0,1,2,3,4,5,6]"
+    let functionList = localStorage.getItem('functionList')? localStorage.getItem('functionList'):"[0,1,2,3,4,5,6,7]"
     //const functionList = useSelector(state=>state.functionList)
     const [items, setItems] = useState(JSON.parse(functionList));
     const onSortEnd = ({oldIndex, newIndex}) => {
@@ -54,7 +55,6 @@ export default function FunctionAera(){   //中间的功能组件，放在里面
         // </div>
         <div style={{display:display}} className='functionAera'>
         <SortableList axis='xy' items={items} onSortEnd={onSortEnd} />
-        
         </div>
     )
 }
