2bb8ac5754fde6e5a1bf8436ad25e6b8bce2ec0a
wisdompandamaster <2532569059@qq.com>
2022-04-28 20:26:14 +0800

    add competition css

--- src/components/Competition/Competition.js
+++ src/components/Competition/Competition.js
@@ -4,7 +4,13 @@ export default function Competition(){
     return (
         <>
         <div className='competition'>
-            <div className='left'><div></div><p>比赛信息</p></div>
+            <div className="com_left"><div></div><p>比赛信息</p></div>
+            <div className="com_right">
+                <span>NBA</span>
+                <span>LOL</span>
+                <span>围棋</span>
+            </div>
+            <div className="com_board"></div>
         </div>
         </>
     )
