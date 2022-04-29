73738a1cb404dcdf6369113eaf1ac996aa1e9435
wisdompandamaster <2532569059@qq.com>
2022-04-29 13:26:27 +0800

    add nba info

--- src/components/Competition/Competition.js
+++ src/components/Competition/Competition.js
@@ -1,4 +1,43 @@
 import './Competition.css'
+import defaultSetting from '../../config';
+import { useEffect, useState } from 'react'
+
+
+function NBA(){
+
+    const [games,setGames] = useState([])
+    useEffect(()=>{
+        let url = "https://china.nba.cn/stats2/scores/miniscoreboard.json?countryCode=CN&locale=zh_CN&tz=%2B8"
+        async function getGameList(){   
+            fetch(url).then((response)=>response.json())
+            .then((data)=>{ setGames(data.payload.today.games)}
+            ).catch((e)=>console.log("error"));
+        }
+        getGameList() 
+    },[])
+
+    return (
+        <div>
+        {
+            games.map((item,index)=>{
+              let awayTeam = item.awayTeam.profile.name
+              let homeTeam = item.homeTeam.profile.name
+              return (
+                <div key={index} className="nba">
+                <span className="nba_team"><img src={defaultSetting.imgSite + "nbalogo/" + awayTeam + '.png'}/>{awayTeam}</span>
+                <span className="score_time">{item.boxscore.awayScore} : {item.boxscore.homeScore}
+                <div>{item.boxscore.statusDesc}</div>
+                <div>{item.seriesText}</div>
+                </span>
+                <span className="nba_team"><img src={defaultSetting.imgSite + "nbalogo/" + homeTeam + '.png'}/>{homeTeam}</span>
+                </div>
+              )
+            })
+            
+        }
+        </div>
+    )
+}
 
 export default function Competition(){
     return (
@@ -10,7 +49,11 @@ export default function Competition(){
                 <span>LOL</span>
                 <span>围棋</span>
             </div>
-            <div className="com_board"></div>
+            <div className="com_board">
+                <a href='https://china.nba.cn/' target='_blank'><NBA/></a>
+                {/* <div></div>
+                <div></div> */}
+            </div>
         </div>
         </>
     )
