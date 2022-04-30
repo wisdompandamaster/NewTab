2b95ee6916952c2db1a0f3cad90f31dd03a53d36
wisdompandamaster <2532569059@qq.com>
2022-05-01 03:26:00 +0800

    fix(Competition):show info if no games today

--- src/components/Competition/Competition.js
+++ src/components/Competition/Competition.js
@@ -4,13 +4,13 @@ import { useEffect, useState } from 'react'
 
 
 function NBA(){
-
+    
     const [games,setGames] = useState([])
     useEffect(()=>{
         let url = "https://china.nba.cn/stats2/scores/miniscoreboard.json?countryCode=CN&locale=zh_CN&tz=%2B8"
         async function getGameList(){   
             fetch(url).then((response)=>response.json())
-            .then((data)=>{ setGames(data.payload.today.games)}
+            .then((data)=>{ setGames(data.payload.today.games)}         //.next.games 表示下一天
             ).catch((e)=>console.log("error"));
         }
         getGameList() 
@@ -27,45 +27,49 @@ function NBA(){
        }
     },[])
 
+    const have_game = games.map((item,index)=>{
+        let awayTeam = item.awayTeam.profile.name
+        let homeTeam = item.homeTeam.profile.name
+        let time = new Date(item.profile.dateTimeEt)
+        time = new Date(time.setHours(time.getHours() + 12))
+        time = time.getHours() + " : " + time.toLocaleTimeString().slice(3,5)
+        let score = item.boxscore.awayScore + " - " + item.boxscore.homeScore
+        let score_time = item.boxscore.status !== "1" ?  score:time
+        return (
+          <div key={index} className="nba">
+          <span className="nba_team"><img alt='logo' src={defaultSetting.imgSite + "nbalogo/" + awayTeam + '.png'}/>{awayTeam}</span>
+          <span className="score_time">
+              {score_time}
+              <div style={{color:"red"}}>{item.boxscore.statusDesc}</div>
+              <div>{item.seriesText}</div>
+              {/* <div>{time}</div> */}
+          </span>
+          <span className="nba_team"><img alt='logo' src={defaultSetting.imgSite + "nbalogo/" + homeTeam + '.png'}/>{homeTeam}</span>
+          </div>
+        )
+      })
+
+    const no_game = <div style={{fontSize:"30px", height:"120px",width:"100%",textAlign:"center",lineHeight:"110px",fontWeight:"700",color:"#00000033",letterSpacing:"8px"}}>今日无赛程</div>
+    
     return (
         <div>
         {
-            games.map((item,index)=>{
-              let awayTeam = item.awayTeam.profile.name
-              let homeTeam = item.homeTeam.profile.name
-              let time = new Date(item.profile.dateTimeEt)
-              time = new Date(time.setHours(time.getHours() + 12))
-              time = time.getHours() + " : " + time.toLocaleTimeString().slice(3,5)
-              let score = item.boxscore.awayScore + " : " + item.boxscore.homeScore
-              let score_time = item.boxscore.status !== "1" ?  score:time
-              return (
-                <div key={index} className="nba">
-                <span className="nba_team"><img alt='logo' src={defaultSetting.imgSite + "nbalogo/" + awayTeam + '.png'}/>{awayTeam}</span>
-                <span className="score_time">
-                    {score_time}
-                    <div style={{color:"red"}}>{item.boxscore.statusDesc}</div>
-                    <div>{item.seriesText}</div>
-                    {/* <div>{time}</div> */}
-                </span>
-                <span className="nba_team"><img alt='logo' src={defaultSetting.imgSite + "nbalogo/" + homeTeam + '.png'}/>{homeTeam}</span>
-                </div>
-              )
-            })
-            
+            games.length === 0 ? no_game : have_game 
         }
         </div>
     )
 }
 
 export default function Competition(){
+    const [type,setType] = useState(0)          //比赛类型
     return (
         <>
         <div className='competition'>
             <div className="com_left"><div></div><p>比赛信息</p></div>
             <div className="com_right">
-                <span>NBA</span>
-                <span>LOL</span>
-                <span>围棋</span>
+                <span onMouseOver={()=>setType(0)} style={{backgroundColor:(type===0? '#00000022':'#ffffff')}}>NBA</span>
+                <span onMouseOver={()=>setType(1)} style={{backgroundColor:(type===1? '#00000022':'#ffffff')}}>LOL</span>
+                <span onMouseOver={()=>setType(2)} style={{backgroundColor:(type===2? '#00000022':'#ffffff')}}>围棋</span>
             </div>
             <div className="com_board">
                 <a href='https://china.nba.cn/' rel="noreferrer" target='_blank'><NBA/></a>
