90c22fe4285d64d91ee749f4e28c4410664d666d
wisdompandamaster <2532569059@qq.com>
2022-05-09 23:46:57 +0800

    feat(SetFooter):add a function to set footer

--- src/components/MottoFooter/SetFooter/SetFooter.js
+++ src/components/MottoFooter/SetFooter/SetFooter.js
@@ -1,15 +1,39 @@
 import React from "react";
-import { Button } from 'antd';
+import { Button, Select, Typography, Divider } from 'antd';
+ 
 
 export default function SetFooter(){
+
+    const options = [];
+    for (let i = 0; i < 100000; i++) {
+      const value = `${i.toString(36)}${i}`;
+      options.push({
+        value,
+        disabled: i === 10,
+      });
+    }
+    
+    function handleChange(value) {
+      console.log(`selected ${value}`);
+    }
+
+
     return (
       <div>
       <span style={{marginRight:'60px'}}>编辑格言</span>
-      <Button 
+      {/* <Button 
         type="dash"
       >
        设置内容
-      </Button>
+      </Button> */}
+      <Select
+      mode="multiple"
+      style={{ width: '100%' }}
+      placeholder="Please select"
+      defaultValue={['a10', 'c12']}
+      onChange={handleChange}
+      options={options}
+    />
       </div>
     )
 }
