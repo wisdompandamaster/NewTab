9f2283c335ccb27348ea7b7d1fd5fb0d720f6d51
wisdompandamaster <2532569059@qq.com>
2022-05-18 10:51:49 +0800

    feat(CountDown):continue to complete the countdown

--- src/components/CountDown/CountDown.js
+++ src/components/CountDown/CountDown.js
@@ -2,13 +2,25 @@ import './CountDown.css'
 
 //一个想法，把顶部的时间变成倒计时，可选
 export default function CountDown(){
+
+    const now = new Date()
+    //const nowDate = now.getHours().toLocaleString();
+    const deadline = new Date('2022-12-24 00:00')
+    const timeRemainning = deadline - now;
+    let day, hour, minute, second;
+    
+    second = Math.floor(timeRemainning / 1000 % 60)     //用余数来把毫秒转化为可表示的时间
+    minute = Math.floor(timeRemainning / 1000 / 60 % 60)
+    hour = Math.floor(timeRemainning / 1000 / 60 / 60 % 24)
+    day = Math.floor(timeRemainning / 1000 / 60 / 60 / 24) + 1
+
     return (
         <>
         <div className='CountDown'>
             <div className='left'><div></div><p>倒计时</p></div>
             <div className='countdown_content'>
                 <div>距离考研还剩</div>
-                <div>123<span>days</span></div>
+                <div>{day}<span>days</span></div>
             </div>
         </div>
         </>
