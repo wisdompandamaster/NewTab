c49ac464b084aed4b58c93f0a1b90e1b7d532734
wisdompandamaster <2532569059@qq.com>
2022-05-20 10:21:21 +0800

    feat(CountDown):function to add countdown

--- src/components/CountDown/CountDown.js
+++ src/components/CountDown/CountDown.js
@@ -1,11 +1,17 @@
 import './CountDown.css';
 import { Modal } from 'antd';
 import { useState } from 'react';
+import useLocalStorage from "../../hooks/useLocalStorage";
 
 //一个想法，把顶部的时间变成倒计时，可选
 export default function CountDown(){
 
-    const countdownList = [{'name':'考研','ddl':'2022-12-24 00:00'},{'name':'答辩','ddl':'2022-5-24 00:00'}]
+    const temp =  [{'name':'考研','ddl':'2022-12-24 00:00'},{'name':'答辩','ddl':'2022-5-24 00:00'}]
+
+    //添加localstorage支持
+    const [countdownList, setcountdownList] = useLocalStorage('countdownList',[])
+   
+    const [countdown, setcountdown] = useState(countdownList)
 
     const [isModalVisible, setIsModalVisible] = useState(false);
 
@@ -34,7 +40,7 @@ export default function CountDown(){
             <div className='left'><div></div><p>倒计时</p></div>
             <div className='countdown_content'>
             {
-                countdownList.map((item)=>{
+                countdown.map((item)=>{
                     const timeRemainning = new Date(item.ddl) - now;
                     const day = Math.floor(timeRemainning / 1000 / 60 / 60 / 24) + 1
                     return (
@@ -49,7 +55,17 @@ export default function CountDown(){
            </div>
            </div>
         <Modal title={<div style={{fontSize:'30px',letterSpacing:'10px',marginLeft:'54px'}}>倒计时设置</div>} visible={isModalVisible}  width={'600px'}  footer={null}  onCancel={handleCancel}>
-         
+        {   //这里等着用列表组件来添加
+            countdown.map((item)=>{
+                return (
+                <div>
+                <span>{item.name}</span>
+                <span>{item.ddl}</span>
+                </div>
+                )
+            })
+        }
+        <div>hello</div>
         </Modal>   
         </>
     )
