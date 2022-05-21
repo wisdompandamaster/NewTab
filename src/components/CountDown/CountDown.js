4f3ef11df8139f9d4a8d1e9f8eb4db888ae29193
wisdompandamaster <2532569059@qq.com>
2022-05-21 12:16:01 +0800

    feat(CountDown):continue to comlete the new feat

--- src/components/CountDown/CountDown.js
+++ src/components/CountDown/CountDown.js
@@ -18,6 +18,7 @@ export default function CountDown(){
     const showModal = () => {
         setIsModalVisible(true);
     };
+    
     const handleCancel = () => {
         setIsModalVisible(false);
     };
