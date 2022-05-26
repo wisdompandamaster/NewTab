fd7ed834f3c8d3802d5c3d0e2407b66c288f271a
wisdompandamaster <2532569059@qq.com>
2022-05-26 14:34:55 +0800

    feat(SetFooter):continue to change the way to set footer kinds

--- src/components/MottoFooter/SetFooter/SetFooter.js
+++ src/components/MottoFooter/SetFooter/SetFooter.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Switch } from 'antd';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {  Select } from 'antd';
   
  
@@ -9,6 +9,7 @@ export default function SetFooter(){
    
     const footer_api_list = [{value:'动画',id:'a'},{value:'漫画',id:'b'},{value:'游戏',id:'c'},{value:'文学',id:'d'},{value:'原创',id:'e'},{value:'来自网络',id:'f'},{value:'其它',id:'g'},{value:'影视',id:'h'},{value:'诗词',id:'i',disabled:true},{value:'网易云',id:'j'},{value:'哲学',id:'k'},{value:'抖机灵',id:'l'}]
     
+    const footerexist = useSelector(state=>state.footerexist)
     const dispatch = useDispatch()
 
     function handleChange(value) {
@@ -31,19 +32,18 @@ export default function SetFooter(){
         type: 'CHANGE_FOOTEREXIST',
         footerexist: checked
       })
-      console.log(`switch to ${checked}`);
+      //console.log(`switch to ${checked}`);
     }
 
 
     return (
-      <div>
-      <span style={{marginRight:'60px'}}>编辑格言</span>
-      {/* 设置格言是否显示,计划通过滑动swiper来实现*/ }
+      <div style={{display:'flex',alignItems:'center'}}>
+      <span style={{marginRight:'10px'}}>编辑格言</span>
       {/* 下次更新完设置格言这一部分 5.23 */}
       <Select
       mode="multiple"
       style={{
-        width: '40%',
+        width: '58%',
         //height:'25px',
         //overflowY:'scroll'
       }}
@@ -51,6 +51,10 @@ export default function SetFooter(){
       defaultValue={['诗词', '网易云','哲学']}
       onChange={handleChange}
       options={footer_api_list}
+      disabled={!footerexist}
+      maxTagCount={2}
+      maxTagTextLength={1}
+      listHeight='200px'
       />
       <Switch defaultChecked onChange={onChange} />   
 
