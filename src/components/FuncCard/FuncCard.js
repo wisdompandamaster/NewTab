7a7e010c56ad464083818d412b0a06b026ce5289
wisdompandamaster <2532569059@qq.com>
2022-06-11 23:52:30 +0800

    refactor(CSS):package a card component

--- src/components/FuncCard/FuncCard.js
+++ src/components/FuncCard/FuncCard.js
@@ -1,10 +1,13 @@
-import { Card } from 'antd'
 
 import './FuncCard.css'
 
 export default function FuncCard(props){
+
+    const {title} = props
+
     return (
         <div className='funcCard'>
+            <div>{title}</div>
             {props.children}
         </div>
     )
