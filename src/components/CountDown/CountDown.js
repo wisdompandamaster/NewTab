c0275164fc59ec4a1082fd23b10daa2714a223a1
wisdompandamaster <2532569059@qq.com>
2022-07-07 17:41:20 +0800

    refactor(CountDown):add a reminder

--- src/components/CountDown/CountDown.js
+++ src/components/CountDown/CountDown.js
@@ -146,8 +146,27 @@ export default function CountDown(){
 
     const handleWheelCapture = (e) => {
         // e.preventDefault();
-        e.stopPropagation();
+        if(dataSource.length >= 2)
+        {
+          e.stopPropagation();
+        }
+        
     } 
+
+    const no_countdown = <div style={{fontSize:"30px", height:"120px",width:"100%",textAlign:"center",lineHeight:"110px",fontWeight:"700",color:"#00000033",letterSpacing:"8px"}}>添加倒计时</div>
+
+    const have_countdown = dataSource.map((item)=>{
+        const timeRemainning = new Date(item.ddl) - now;
+        const day = Math.floor(timeRemainning / 1000 / 60 / 60 / 24) + 1
+        return (
+                    <div key={item.id}>
+                    <div>距离{item.name}还剩</div>
+                    <div>{day}<span>days</span></div>
+                    </div>
+            //继续写countdown 变换的代码
+        )
+       
+    })
 
     return (
         <>
@@ -161,35 +180,9 @@ export default function CountDown(){
         >
             {/* <div className='left'><div></div><p>倒计时</p></div> */}
             <div className='countdown_content' onClick={showModal} onWheelCapture={handleWheelCapture}>
-            {/* <Swiper className='swiper-no-swiping' 
-                            spaceBetween={0}
-                            slidesPerView={1}
-                            //loop={true}
-                            onSlideChange={() => console.log('slide change')}
-                            onSwiper={(swiper) => console.log(swiper)}
-                            //pagination={{ clickable: true }}
-                            //scrollbar={{ draggable: false }}
-                            
-                            //mousewheel={true}
-                            navigation={true}
-                            //loop={true}
-                            modules={[Pagination,Navigation]}
-                        > */}
-            {
-                dataSource.map((item)=>{
-                    const timeRemainning = new Date(item.ddl) - now;
-                    const day = Math.floor(timeRemainning / 1000 / 60 / 60 / 24) + 1
-                    return (
-                                <div key={item.id}>
-                                <div>距离{item.name}还剩</div>
-                                <div>{day}<span>days</span></div>
-                                </div>
-                        //继续写countdown 变换的代码
-                    )
-                   
-                })
+            { 
+                dataSource.length > 0 ? have_countdown:no_countdown
             }
-             
            </div>
            </FuncCard>
         <Modal title={<div style={{fontSize:'30px',letterSpacing:'10px',marginLeft:'54px'}}>倒计时设置</div>} visible={isModalVisible}  width={'1000px'}  footer={null}  onCancel={handleCancel}>
