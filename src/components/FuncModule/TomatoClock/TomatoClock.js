d6efa7674591e31a8f8e12f40f72bed67496cde2
wisdompandamaster <2532569059@qq.com>
2022-08-30 10:03:13 +0800

    feat(TomatoClock):continue to complete the component

--- src/components/FuncModule/TomatoClock/TomatoClock.js
+++ src/components/FuncModule/TomatoClock/TomatoClock.js
@@ -1,16 +1,37 @@
 import './TomatoClock.css'
 import FuncCard from '../../FuncCard/FuncCard'
+import FuncModal from '../../FuncModal/FuncModal'
+import { memo, useState } from 'react'
 
 function TomatoClock(){
+
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+    
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+  
+
     return (
         <FuncCard
          title = "番茄时钟"
         >
-         <div>
+         <div style={{height:'100%'}} onClick={showModal}>
+            hello
          </div>
+        <FuncModal
+          title={<div style={{fontSize:'30px',letterSpacing:'10px'}}>设置番茄钟</div>} visible={isModalVisible}  width={'600px'} onCancel={handleCancel}
+        >
+            
+        </FuncModal>
+         
         </FuncCard>
     )
 } 
 
 
-export default TomatoClock;
+export default memo(TomatoClock);
