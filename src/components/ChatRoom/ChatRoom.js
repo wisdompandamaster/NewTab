b1fab1b4a0abb70fa4de53bd0a55bcd2367065b7
wisdompandamaster <2532569059@qq.com>
2023-06-30 11:19:27 +0800

    feat(ChatRoom):continue to setup chatroom

--- src/components/ChatRoom/ChatRoom.js
+++ src/components/ChatRoom/ChatRoom.js
@@ -1,8 +1,37 @@
 import "./ChatRoom.css";
+import FuncModal from "../FuncModal/FuncModal";
+import { useState } from "react";
+import { Button } from "antd";
 import { memo } from "react";
 
 function ChatRoom() {
-  return <div>聊天系统开建...</div>;
+  // modal组件控制函数
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const showModal = () => {
+    // openNotification();
+    setIsModalVisible(true);
+  };
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  return (
+    <>
+      <Button type='primary' ghost onClick={showModal}>
+        聊天系统开建...
+      </Button>
+      <FuncModal
+        bodyStyle={{ padding: "11px" }}
+        width={"45%"}
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      ></FuncModal>
+    </>
+  );
 }
 
 export default memo(ChatRoom);
