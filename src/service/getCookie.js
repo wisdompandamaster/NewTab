32e323e84d917d55b478d2ce3ca06f1d908b36fc
wisdompandamaster <2532569059@qq.com>
2024-03-26 16:30:03 +0800

    inint

--- src/service/getCookie.js
+++
@@ -1,9 +0,0 @@
-export default function getCookie(name) {
-  let arr;
-  const reg = new RegExp(`(^| )${name}=([^;]*)(;|$)`);
-  if ((arr = document.cookie.match(reg))) {
-    return unescape(arr[2]);
-  } else {
-    return "";
-  }
-}
--- src/store/index.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from "redux";
-import reducer from "./reducer";
-
-const configureStore = () => createStore(reducer);
-
-export default configureStore;
