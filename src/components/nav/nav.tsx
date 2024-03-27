c6e28a537c8cafb7ed38e3611149816a1c44545c
wisdompandamaster <2532569059@qq.com>
2024-03-27 08:16:21 +0800

    feat(nav):add hover card to time

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -21,15 +21,28 @@ const Nav = () => {
               <AvatarFallback>CN</AvatarFallback>
             </Avatar>
           </HoverCardTrigger>
-          <HoverCardContent align="start" sideOffset={10} className="w-44 h-52">
-            UserName
+          <HoverCardContent align="start" sideOffset={10} className="w-44 h-48">
+            <p className="text-gray-500 font-semibold text-center">
+              wisdompanda
+            </p>
+            <hr className="mt-3" />
+            <p>个人登录信息</p>
           </HoverCardContent>
         </HoverCard>
       </div>
-      <div className="fixed top-5 right-20 text-cyan-50 leading-8 h-8 text-lg font-semibold cursor-pointer ring-2 ring-white px-3 rounded-sm hover:bg-[#0007]">
-        <p>11 : 30 : 25</p>
+      <div className="fixed top-5 right-20 cursor-pointer">
+        <HoverCard>
+          <HoverCardTrigger>
+            <p className=" text-cyan-50 leading-8 h-8 text-lg font-semibold  ring-2 ring-white px-3 rounded-sm hover:bg-[#0005]">
+              11 : 30 : 25
+            </p>
+          </HoverCardTrigger>
+          <HoverCardContent align="end" sideOffset={10} className="w-56 h-48">
+            世界时间
+          </HoverCardContent>
+        </HoverCard>
       </div>
-      <div className="fixed top-4 right-6 cursor-pointer w-10 h-10 flex justify-center items-center rounded-sm hover:bg-[#0007]">
+      <div className="fixed top-4 right-6 cursor-pointer w-10 h-10 flex justify-center items-center rounded-sm hover:bg-[#0005]">
         <Settings color="white" className="w-8 h-8" />
       </div>
     </div>
